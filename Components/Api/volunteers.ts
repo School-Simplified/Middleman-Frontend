fa46fe33665eb8e9b3caba2a5f2a2b45ee42ecff
josef <macerajosef@gmail.com>
2022-07-12 13:30:41 -0700

    added google email creation client-side

--- Components/Api/volunteers.ts
+++ Components/Api/volunteers.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API_URL = "http://127.0.0.1:8000";
-const scriptId = "16CDtcdnJV_2MxXdRUXRLmfWMm1cUewpzzFCd2QOf07BPXvQJx4LXxdh0"
+const scriptId = "16CDtcdnJV_2MxXdRUXRLmfWMm1cUewpzzFCd2QOf07BPXvQJx4LXxdh0";
 type Volunteer = {
   fullName: string;
   strikes?: number;
@@ -22,9 +22,9 @@ export const getVolunteers = async () => {
 };
 
 export const getVolunteerByID = async (id: number) => {
-  const response = await axios.get(`${API_URL}/api/volunteers/${id}`)
+  const response = await axios.get(`${API_URL}/api/volunteers/${id}`);
   return response.data;
-}
+};
 
 export const deleteVolunteer = async (id: number) => {
   const response = await axios.delete(`${API_URL}/api/volunteers/${id}`);
@@ -32,24 +32,52 @@ export const deleteVolunteer = async (id: number) => {
 };
 
 export const createVolunteer = async (data: any) => {
+  console.log(data);
+  const token = localStorage.getItem("token");
   const response = await axios.post(`${API_URL}/api/volunteers`, data);
+  const familyName = data.fullName.split(" ")[1];
+  const givenName = data.fullName.split(" ")[0];
+  const googleData = {
+    primaryEmail: data.orgEmail,
+    password: "schoolsimplified",
+    name: { familyName, givenName },
+    changePasswordAtNextLogin: true,
+  };
+  const googleResponse = await axios.post(
+    `https://script.googleapis.com/v1/scripts/${scriptId}:run`,
+    {
+      function: "createUser",
+      devMode: true,
+      parameters: [googleData],
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  console.log(googleResponse);
   return response.data;
 };
 
 export const updateVolunteer = async (id: number, data: any) => {
-  const response = await axios.put(`${API_URL}/api/volunteers/${id}`, data)
+  const response = await axios.put(`${API_URL}/api/volunteers/${id}`, data);
   return response.data;
-}
+};
 
 export const getGoogleVolunteers = async () => {
-  const token = localStorage.getItem("token")
+  const token = localStorage.getItem("token");
   const header = {
-    headers: { Authorization: `Bearer ${token}` }
-  }
+    headers: { Authorization: `Bearer ${token}` },
+  };
   const body = {
     function: "listAllUsers",
-    devMode: "true"
-  }
-  const response = await axios.post(`https://script.googleapis.com/v1/scripts/${scriptId}:run`, body, header);
+    devMode: "true",
+  };
+  const response = await axios.post(
+    `https://script.googleapis.com/v1/scripts/${scriptId}:run`,
+    body,
+    header
+  );
   return response.data;
 };
