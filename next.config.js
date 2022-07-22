b57b6c8709c9004b8753ee1e664b8c7d5ba0a6a5
josef <macerajosef@gmail.com>
2022-07-22 14:34:31 -0700

    sentry: init config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const moduleExports = {
   reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+const sentryWebpackPluginOptions = {
+  silent: true, // Suppresses all logs
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
